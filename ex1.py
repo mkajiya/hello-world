579f7bfb304ff0ea0bdaacfb14dab99d5d95e544
mkajiya <mariokajiya@gmail.com>
2015-09-25 11:48:08 -0300

    Adicionando novos exemplos

---
+++ ex1.py
@@ -0,0 +1,3 @@
+print "My name is Mario"
+print "Where is my semicolons?"
+#print "Help me, I'm trapped here"
---
+++ ex2.py
@@ -0,0 +1,5 @@
+#this is a comment
+print "this is not a comment" #this is a comment
+print "this is not a comment too"
+#print this is a comment
+print "this is dumb"
