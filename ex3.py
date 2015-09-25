e6c15fa0d929fb9472e1f4dc4ff8bcc50737ab51
mkajiya <mariokajiya@gmail.com>
2015-09-25 15:09:31 -0300

    Mais coisas do learn python hard way

---
+++ ex3.py
@@ -0,0 +1,8 @@
+print "Some string here"
+print "sum", 25 + 30 / 6
+print "other sum", 100 - 25 * 3 % 4
+print 1 > 2
+print 3 + 2 < 5 - 7
+print "ok, i know about operators, what i'm doing?"
+print "maybe this is true", 3 > 7
+print "ok, enough"
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'mario'
+my_age = 27
+my_height = 170 #cm
+my_weight = 90 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s" % my_name
+print "I'm %d cms tall" % my_height
+print "I'm heavy. I have %d kgs" % my_weight
+print "Yeah, that's heavy"
+print "My hair is %s and my eyes are %s" % (my_hair, my_eyes)
+print "And my teeth is %s" % my_teeth
+
+print "If I add %d, %d and %d I get %d" % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
