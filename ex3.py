bf98a7e32a8b3aa0159b5a5a1f53562c3edf5fb4
mkajiya <mariokajiya@gmail.com>
2015-09-29 09:33:48 -0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,8 +0,0 @@
-print "Some string here"
-print "sum", 25 + 30 / 6
-print "other sum", 100 - 25 * 3 % 4
-print 1 > 2
-print 3 + 2 < 5 - 7
-print "ok, i know about operators, what i'm doing?"
-print "maybe this is true", 3 > 7
-print "ok, enough"
